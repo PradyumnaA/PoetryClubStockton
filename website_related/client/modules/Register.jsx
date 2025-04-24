98ff9893b99a0f66b2728ffca37c72947d5726fa
Pradyumna Anil Kunmar  Kubear <pradyumkubeara@gmail.com>
2025-04-23 19:46:27 -0700

    Created dummy components for login and registeration

---
+++ website_related/client/modules/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Register Page</h2>
+      <p>This is the registration component</p>
+    </div>
+  );
+};
+
+export default Register;
+
---
+++ website_related/client/modules/index.js
@@ -0,0 +1,5 @@
+// modules/index.js
+import Login from './Login';
+import Register from './Register';
+
+export { Login, Register };
