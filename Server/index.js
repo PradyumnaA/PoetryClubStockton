48ad432bcd027043bac8e0e13594e046ceeaee63
Pradyumna Anil Kunmar  Kubear <pradyumkubeara@gmail.com>
2025-05-04 21:29:02 -0700

    client and server and mognodb completed

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 5000; // fallback if PORT not set
+//Hey resolve later
+app.use(cors({
+    
+}
+
+));
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hi Prada");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
